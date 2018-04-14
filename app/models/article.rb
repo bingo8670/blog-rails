ac547833b3f44abc3a0213e876f70165c00127ff
bingo8670 <bingo891111@gmail.com>
2018-04-14 10:17:07 +0800

    add comment for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  # dependent: :destroy ，可保证删除文章的时候评论也会删除，节省数据库空间；
   validates :title, presence: true,
                     length: { minimum: 5}
 end
